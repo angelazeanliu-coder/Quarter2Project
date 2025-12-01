0fd5f1866c327178171fdc10bbadc207dc3049ef
Anri <angelazeanliu@gmail.com>
2025-12-01 14:06:07 -0800

    asteroid update

--- Asteroid.java
+++ Asteroid.java
@@ -1,14 +1,61 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.util.Random;
+
 public class Asteroid {
-    public int getX(){
+
+    private Image asteroidImage;
+    private int x, y;
+    private int width = 40;
+    private int height = 40;
+    private int dy;
+    private boolean isActive = true;
+    
+    private static final Random RANDOM = new Random();
+    
+    public Asteroid(int minX, int maxX, int speed) {
+        int type = RANDOM.nextInt(4) + 1; 
         
+        try {
+            asteroidImage = ImageIO.read(new File("Asteroid" + type + ".png"));
+        } catch (IOException e) {
+            System.out.println("Image Load Error Asteroid: " + e.getMessage());
+        }
+
+        this.x = minX + RANDOM.nextInt(maxX - minX - width);
+        this.y = -height;
+        this.dy = speed;
     }
-     public int getY(){
-        
+
+    public void update() {
+        if (isActive) {
+            y += dy;
+            
+            if (y > GamePanel.SCREEN_HEIGHT) {
+                isActive = false;
+            }
+        }
     }
-     public int getWidth(){
-        
+
+    public void draw(Graphics g) {
+        if (isActive && asteroidImage != null) {
+            g.drawImage(asteroidImage, x, y, width, height, null);
+        }
     }
-     public int getLength(){
-        
+    
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+    
+    public boolean isActive() {
+        return isActive;
+    }
+    
+    public void destroy() {
+        this.isActive = false;
     }
 }
